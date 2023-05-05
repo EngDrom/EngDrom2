a48264a33abb036c5941d5c4472f3d6049052ca7
Itaï <salutevg@gmail.com>
2023-05-05 18:17:30 +0200

    update editor

--- webdrom/editor/index.js
+++ webdrom/editor/index.js
@@ -48,6 +48,11 @@ class HomeProjectPage extends ProjectPage {
             { "text": "Webdrom", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  },
             { "text": "Level", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  }
         ] });
+        let tree2 = new MExplorer (this, { "text": "Explorer", "components": [
+            { "text": "Project", "component": (parent) => new FileTree(parent).render(), "icons": []  },
+            { "text": "Webdrom", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  },
+            { "text": "Level", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  }
+        ] });
         let property_tree = new MExplorer(this, {
             "text": "Properties",
             "components": [
@@ -66,53 +71,142 @@ class HomeProjectPage extends ProjectPage {
             new Onglet().render()
         ])
 
-        let texteditor = createElement("div",{},"w-full h-full bg-Vwebdrom-light-background p-4 absolute top-8 right-0 text-white z-10 select-none text-xl",[])
-
-        let textarea=createElement("textarea", {}, "w-full h-full bg-Vwebdrom-light-background p-4  absolute text-none top-8 right-0",[])
-
-        textarea.addEventListener("keydown",(e) => {
-            if (e.key == 'Tab') {
-              e.preventDefault();
-              var start = textarea.selectionStart;
-              var end = textarea.selectionEnd;
+        let texteditor = createElement("div",{},"w-full min-h-8 bg-Vwebdrom-background p-4 absolute top-8 right-0 z-10 select-none text-base opacity-.5",[])
+        function createTextArea (value) {
+            let area   = createElement("textarea", {}, "w-full bg-Vwebdrom-background p-4 text-white absolute top-8 right-0", []);
+            area.value = value  
           
-              // set textarea value to: text before caret + tab + text after caret
-              texteditor.innerHTML = texteditor.innerHTML.substring(0, start) +
-                "    " + texteditor.innerHTML.substring(end);
-          
-              // put caret at right position again
-              textarea.selectionStart =
-              textarea.selectionEnd = start + 4;
+            let resize = () => {
+              area.style.height = "5px";
+              area.style.height = (area.scrollHeight + 30) + "px";
             }
-            texteditor.innerHTML += String.fromCharCode(e.keyCode)
+            area.addEventListener("input", resize)
+            area.addEventListener("change", resize)
+            resize()
+          
+            return area
+          }
+          
+        let textarea=createTextArea("")
+        textarea.addEventListener("keydown",(e) => {
+            //console.log(e,textarea.value)
+            if (e.key == 'Tab') {
+                e.preventDefault();
+                var start = textarea.selectionStart;
+                var end = textarea.selectionEnd;
+            
+                // set textarea value to: text before caret + tab + text after caret
+                textarea.value = textarea.value.substring(0, start) +
+                  "    " + textarea.value.substring(end);
+            
+                // put caret at right position again
+                textarea.selectionStart =
+                  textarea.selectionEnd = start + 4;
+            }
+            if(e.key=="Backspace" && texteditor.innerHTML.substr(-8)=="<br><br>"){
+                //alert("remove line")
+                texteditor.innerHTML=texteditor.innerHTML.substr(0,texteditor.innerHTML.length-4)
+                //console.log(texteditor.innerHTML)
+            }
+            //e.preventDefault();
+            //console.log("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890=+}{[]()-_:;., ".search(e.key))
+            /*if("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890=\+}{[]()-_:;.,/ \n".search(e.key) >=0){
+            textarea.value += e.key
+            }*/
             setTimeout(() => {
-                let line_number = texteditor.innerHTML.substr(0, texteditor.selectionStart).split("\n").length
-                let pos0 = texteditor.innerHTML.substr(0, texteditor.selectionStart).length;
-                let line = texteditor.innerHTML.substr(0, texteditor.selectionStart).split("\n")[line_number-1]
-                let lexer = new Dromadaire.Lexer(new Dromadaire.File(line,"index.dmd")).build()
+                let line_number = textarea.value.substr(0, textarea.selectionStart).split("\n").length
+                let pos = 0;
+                for(let i=0;i<line_number;i++){
+                    pos+=4+texteditor.innerHTML.split("<br>")[i].length
+                }
+                /*if(line_number>0){
+                    pos-=4
+                }*/
+                let pos0=pos;
+                //console.log(pos0)
+                if(pos>4 && e.key != "Enter"){
+                    pos0=pos-5
+                }
+                //console.log(textarea.value)
+                //console.log(texteditor.innerHTML)
+                //console.log(pos0)
+                while(texteditor.innerHTML.substr(pos0,4)!="<br>" && pos0!=0){
+                    //console.log(texteditor.innerHTML.substr(pos0,pos0+4))
+                    pos0--;
+                }
+                pos0+=4
+                let fin=pos-4;
+                //console.log(pos0)
+                while(texteditor.innerHTML.substr(fin,4)!="<br>" && fin<texteditor.innerHTML.length){
+                    fin++;
+                }
+                //fin+=4
+                //console.log(fin)
+                let line = textarea.value.split("\n")[line_number-1]
+                let lexer = new Dromadaire.Lexer(new Dromadaire.File(line,"index.dmd")).build()[0]
                 let color={
-                    1:"#123456",
-                    2:"#234567",
-                    3:"#345678",
-                    4:"#456789"
+                    1:"#ff0000",
+                    2:"#00ff00",
+                    3:"#0000ff",
+                    4:"#ffff00"
                 }
-                for(let i =0;i<lexer.length;i++){
-                    if(lexer[i].__type==17){
-                        continue;
+                let content=texteditor.innerHTML
+                texteditor.innerHTML=content.substring(0,pos0)
+                let j=0
+                let col=lexer[0].col
+                let max=col+lexer[0].size
+                for(let i=1;i<=line.length;i++){
+                    if(i>=max){
+                        j++
                     }
-                    texteditor.innerHTML = texteditor.innerHTML.substring(0, pos0+lexer[i].column) + "<span style='color:"+color[lexer[i].__type]+"'>"+line.substr(lexer[i].columnn,lexer[i].size)+"</span>"+texteditor.innerHTML.substring(pos0+lexer[i].column+lexer[i].size+35);
-                    pos0+=35;
+                    col=lexer[j].col
+                    max=col+lexer[j].size
+                    /*if(lexer[j].__type==17){
+                        texteditor.innerHTML+="<br>"
+                    }*/
+                    //else{
+                    if(col<=i<max){
+                        if(line[i-1]==" "){
+                        texteditor.innerHTML+="<span style='display:inline-block;line-height:20px;min-width:4.5px;color:"+color[lexer[j].__type]+"'>"+line[i-1]+"</span>"
+                        }
+                        else{
+                            texteditor.innerHTML+="<span style='display:inline-block;line-height:20px;color:"+color[lexer[j].__type]+"'>"+line[i-1]+"</span>"
+                        }
+                    }
+                    else{
+                        if(line[i-1]==" "){
+                            texteditor.innerHTML+="<span style='display:inline-block;min-width:4.5px;line-height:20px;color:#ffffff'>"+line[i-1]+"</span>"
+                            }
+                            else{
+                                texteditor.innerHTML+="<span style='display:inline-block;line-height:20px;color:#ffffff'>"+line[i-1]+"</span>"
+                            }
+                    }
+                //}
                 }
-                console.log(lexer)
+                //console.log(texteditor.innerHTML)
+                if(texteditor.innerHTML.length!=0){
+                texteditor.innerHTML+="<br>"+content.substring(fin+4);
+                }
+                else{
+                    texteditor.innerHTML+=content.substring(fin+4);
+                }
+                //console.log(lexer)
 
             },0)
           })
-
+        
+ 
         let onglets= createElement("div", {}, "w-full h-full relative", [
             tabs,
             texteditor,
             textarea
         ])
+
+        let ongletssplitter = new MSplitter (this, "horizontal", undefined, true,
+        createElement("div", {}, "w-full h-full bg-Vwebdrom-light-background", [
+            tree2.render()
+        ]),
+        onglets)
 
         let splitter = new MSplitter (this, "horizontal", undefined, true,
             createElement("div", {}, "w-full h-full bg-Vwebdrom-light-background", [
@@ -128,25 +222,38 @@ class HomeProjectPage extends ProjectPage {
         let splitter_viewport = new ViewportComponent(
             this, splitter, left_onglet, 21
         )
+        let splitter_onglets_viewport = new ViewportComponent(
+            this, ongletssplitter, left_onglet, 21
+        )
         splitter.sizes     = [ 300, 800, 300 ];
         splitter1.sizes    = [ 300, 300 ];
         splitter.min_sizes = [ 200, 400, 200 ];
         splitter.collapse  = [ true, false, true ];
         splitter1.collapse = [ false, false ];
+        ongletssplitter.sizes = [300,800]
+        ongletssplitter.min_sizes=[200,400]
+        ongletssplitter.collapse=[false,false]
+        var splitter_=splitter_viewport.render()
+        var onglets_view=splitter_onglets_viewport.render()
+        this.contenticon2=createIcon("content_copy", "icon-32")
+        this.iconbar=createElement("div", [], "absolute left-0 top-0 w-[2px] bg-Vwebdrom-editor-text h-full")
+        this.icon2=createElement("div", {onclick:()=>{this.element.removeChild(splitter_);this.element.appendChild(onglets_view);this.icon2.removeChild(this.contenticon2);this.icon2.appendChild(this.iconbar);this.icon2.appendChild(this.contenticon2)}}, "cursor-pointer p-[14px] h-15 relative", [
+            this.contenticon2
+        ])
+        this.contenticon1=createIcon("desktop_windows", "icon-32")
+        this.icon1=createElement("div", {onclick:()=>{this.element.appendChild(splitter_);this.element.removeChild(onglets_view);this.icon1.removeChild(this.contenticon1);this.icon1.appendChild(this.iconbar);this.icon1.appendChild(this.contenticon1)}}, "cursor-pointer p-[14px] h-15 relative", [
+            this.iconbar,
+            this.contenticon1
+        ])
         this.element = createElement("div", {}, "h-full flex", [
             createElement("div", {}, `w-[${left_onglet}px]`, [
-                createElement("div", [], "cursor-pointer p-[14px] h-15 relative", [
-                    createElement("div", [], "absolute left-0 top-0 w-[2px] bg-Vwebdrom-editor-text h-full"),
-                    createIcon("desktop_windows", "icon-32")
-                ]),
-                createElement("div", [], "cursor-pointer p-[14px] h-15", [
-                    createIcon("content_copy", "icon-32")
-                ]),
+                this.icon1,
+                this.icon2,
                 createElement("div", [], "cursor-pointer p-[14px] h-15", [
                     createIcon("schema", "icon-32")
                 ])
             ]),
-            splitter_viewport.render()
+            splitter_
         ])
     }
 
