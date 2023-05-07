22426847ea67c27d44accf0da6707d3a3f68d8e6
Itaï <salutevg@gmail.com>
2023-05-07 09:59:11 +0200

    update editor

--- webdrom/editor/index.js
+++ webdrom/editor/index.js
@@ -46,11 +46,7 @@ class HomeProjectPage extends ProjectPage {
             { "text": "Webdrom", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  },
             { "text": "Level", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  }
         ] });
-        let tree2 = new MExplorer (this, { "text": "Explorer", "components": [
-            { "text": "Project", "component": (parent) => new FileTree(parent).render(), "icons": []  },
-            { "text": "Webdrom", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  },
-            { "text": "Level", "component": (parent) => new MTree(parent, TEST_MTREE_CONFIG).render(), "icons": []  }
-        ] });
+
         let property_tree = new MExplorer(this, {
             "text": "Properties",
             "components": [
@@ -64,10 +60,132 @@ class HomeProjectPage extends ProjectPage {
             this.engine.render(),
             createElement("div", {}, "", [])
         )
+
+        let splitter = new MSplitter (this, "horizontal", undefined, true,
+            createElement("div", {}, "w-full h-full bg-Vwebdrom-light-background", [
+                tree.render()
+            ]),
+            splitter1.render(),
+            createElement("div", {}, "w-full h-full bg-Vwebdrom-light-background", [
+                property_tree.render()
+            ])
+        )
+
+        let splitter_viewport = new ViewportComponent(
+            this, splitter, left_onglet, 21
+        )
+        
+        splitter.sizes     = [ 300, 800, 300 ];
+        splitter1.sizes    = [ 300, 300 ];
+        splitter.min_sizes = [ 200, 400, 200 ];
+        splitter.collapse  = [ true, false, true ];
+        splitter1.collapse = [ false, false ];
+        var splitter_=splitter_viewport.render()
+        this.element = createElement("div", {}, "h-full flex", [
+            splitter_
+        ])
+    }
+
+    _render () {
+        return this.element;
+    }
+}
+
+class ProjectComponent extends Component {
+    constructor (parent, project_page, engine) {
+        super(parent);
+        this.engine = new WebEngine(this.component);
+
+        this.project_pages = {  }
+    
+        this.setPage(project_page, false);
+        this.prompt = new MPromptManager();
+    }
+    setPage (project_type, render = true) {
+        if (this.project_main === project_type) return ;
+
+        if (this.project_pages[project_type] === undefined)
+            this.project_pages[project_type] = (new project_type(this, this.engine)).render();
+
+        this.project_main = project_type;
+        this.project_page = this.project_pages[project_type];
+
+        if (render) this.render(false);
+    }
+
+    createColorElement (condition) {
+        if (condition) return [ createElement("div", [], "absolute left-0 top-0 w-[2px] bg-Vwebdrom-editor-text h-full") ]
+        return [ ]
+    }
+    createIcon (project_type, icon) {
+        let element = createElement("div", [], "cursor-pointer p-[14px] h-15 relative", [
+            ...this.createColorElement(this.project_main === project_type),
+            createIcon(icon, "select-none icon-32")
+        ])
+
+        element.onclick = (ev) => {
+            this.setPage( project_type );
+        }
+
+        return element;
+    }
+    _render () {
+        return createElement("div", {}, "flex-1 flex", [
+            createElement("div", {}, `w-[${left_onglet}px]`, [
+                this.createIcon( HomeProjectPage, "desktop_windows" ),
+                this.createIcon( TextEditor, "content_copy" ),
+                this.createIcon( GraphEditor, "schema" ),
+            ]),
+            this.project_page,
+            this.prompt.render()
+        ]);
+    }
+}
+
+class Project {
+    constructor (body, page = HomeProjectPage) {
+        let customEvent = new CustomEvent( "WebDrom.CreateProject" )
+        customEvent.project = this;
+        this.body = body;
+
+        this.page = page;
+        this.component = new ProjectComponent(undefined, page);
+        this.component.is_dom_root = true;
+        this.component.dom_body = this.body;
+        this.contextmenu = new ContextMenu(this);
+        this.body.appendChild(this.component.render());
+        
+        document.dispatchEvent( customEvent )
+    }
+
+    prompt (config) {
+        this.component.prompt.addPrompt(config)
+    }
+}
+
+document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+})
+
+
+class TextEditor extends ProjectPage{
+    constructor (parent, engine) {
+        super(parent, engine);
+
+        this._first_render();
+    }
+
+    _first_render () {
+        let tree2 = new MExplorer (this, { "text": "Explorer", "components": [
+            { "text": "Project", "component": (parent) => new FileTree(parent).render(), "icons": []  }
+        ] });
+
         let tabs = createElement("div", {}, "h-8 w-full flex",[
             new Onglet().render(),
             new Onglet().render()
         ])
+
+        let config=undefined;
 
         let texteditor = createElement("div",{},"w-full min-h-8 bg-Vwebdrom-background p-4 absolute top-8 right-0 z-10 select-none text-base opacity-.5",[])
         function createTextArea (value) {
@@ -206,124 +324,20 @@ class HomeProjectPage extends ProjectPage {
         ]),
         onglets)
 
-        let splitter = new MSplitter (this, "horizontal", undefined, true,
-            createElement("div", {}, "w-full h-full bg-Vwebdrom-light-background", [
-                tree.render()
-            ]),
-            splitter1.render(),
-            createElement("div", {}, "w-full h-full bg-Vwebdrom-light-background", [
-                property_tree.render()
-            ])
-        )
 
-        let splitter_viewport = new ViewportComponent(
-            this, splitter, left_onglet, 21
-        )
         let splitter_onglets_viewport = new ViewportComponent(
             this, ongletssplitter, left_onglet, 21
         )
-        splitter.sizes     = [ 300, 800, 300 ];
-        splitter1.sizes    = [ 300, 300 ];
-        splitter.min_sizes = [ 200, 400, 200 ];
-        splitter.collapse  = [ true, false, true ];
-        splitter1.collapse = [ false, false ];
         ongletssplitter.sizes = [300,800]
         ongletssplitter.min_sizes=[200,400]
         ongletssplitter.collapse=[false,false]
-        var splitter_=splitter_viewport.render()
         var onglets_view=splitter_onglets_viewport.render()
-        this.contenticon2=createIcon("content_copy", "icon-32")
-        this.iconbar=createElement("div", [], "absolute left-0 top-0 w-[2px] bg-Vwebdrom-editor-text h-full")
-        this.icon2=createElement("div", {onclick:()=>{this.element.removeChild(splitter_);this.element.appendChild(onglets_view);this.icon2.removeChild(this.contenticon2);this.icon2.appendChild(this.iconbar);this.icon2.appendChild(this.contenticon2)}}, "cursor-pointer p-[14px] h-15 relative", [
-            this.contenticon2
-        ])
-        this.contenticon1=createIcon("desktop_windows", "icon-32")
-        this.icon1=createElement("div", {onclick:()=>{this.element.appendChild(splitter_);this.element.removeChild(onglets_view);this.icon1.removeChild(this.contenticon1);this.icon1.appendChild(this.iconbar);this.icon1.appendChild(this.contenticon1)}}, "cursor-pointer p-[14px] h-15 relative", [
-            this.iconbar,
-            this.contenticon1
-        ])
         this.element = createElement("div", {}, "h-full flex", [
-            splitter_
+            onglets_view
         ])
+        //file_tree
     }
-
     _render () {
         return this.element;
     }
 }
-
-class ProjectComponent extends Component {
-    constructor (parent, project_page, engine) {
-        super(parent);
-        this.engine = new WebEngine(this.component);
-
-        this.project_pages = {  }
-    
-        this.setPage(project_page, false);
-        this.prompt = new MPromptManager();
-    }
-    setPage (project_type, render = true) {
-        if (this.project_main === project_type) return ;
-
-        if (this.project_pages[project_type] === undefined)
-            this.project_pages[project_type] = (new project_type(this, this.engine)).render();
-
-        this.project_main = project_type;
-        this.project_page = this.project_pages[project_type];
-
-        if (render) this.render(false);
-    }
-
-    createColorElement (condition) {
-        if (condition) return [ createElement("div", [], "absolute left-0 top-0 w-[2px] bg-Vwebdrom-editor-text h-full") ]
-        return [ ]
-    }
-    createIcon (project_type, icon) {
-        let element = createElement("div", [], "cursor-pointer p-[14px] h-15 relative", [
-            ...this.createColorElement(this.project_main === project_type),
-            createIcon(icon, "select-none icon-32")
-        ])
-
-        element.onclick = (ev) => {
-            this.setPage( project_type );
-        }
-
-        return element;
-    }
-    _render () {
-        return createElement("div", {}, "flex-1 flex", [
-            createElement("div", {}, `w-[${left_onglet}px]`, [
-                this.createIcon( HomeProjectPage, "desktop_windows" ),
-                this.createIcon( (...a) => { throw 'TextEditorPage not developped'; }, "content_copy" ),
-                this.createIcon( GraphEditor, "schema" ),
-            ]),
-            this.project_page,
-            this.prompt.render()
-        ]);
-    }
-}
-
-class Project {
-    constructor (body, page = HomeProjectPage) {
-        let customEvent = new CustomEvent( "WebDrom.CreateProject" )
-        customEvent.project = this;
-        this.body = body;
-
-        this.page = page;
-        this.component = new ProjectComponent(undefined, page);
-        this.component.is_dom_root = true;
-        this.component.dom_body = this.body;
-        this.contextmenu = new ContextMenu(this);
-        this.body.appendChild(this.component.render());
-        
-        document.dispatchEvent( customEvent )
-    }
-
-    prompt (config) {
-        this.component.prompt.addPrompt(config)
-    }
-}
-
-document.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-})
